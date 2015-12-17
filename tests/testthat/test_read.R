f17889f057d637abfd43e63aac8481551406c44f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-17 16:34:12 -0500

    Adding a test case in test_read that can check the read_mutant_selection_data file.

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,6 +1,8 @@
 ### Context: test cases for reading in the data files
 context("read")
 
-test_that("The pipeline.dat file as a total of 165060 (36220) rows of data", {
+test_that("The mutant_selection.dat file as a total of rows of data", {
+  mutant_selection_data <- read_mutant_selection_data()
+  expect_that(nrow(mutant_selection_data), equals(960))
 })
 
