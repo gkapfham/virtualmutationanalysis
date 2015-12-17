6e3fab02e40debd6bdac722b6b3b8d18c1a07c08
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-17 16:23:28 -0500

    Updating the read file with a new read function and appropriate comments.

--- R/read.R
+++ R/read.R
@@ -1,5 +1,10 @@
-#' This is a sample function
+#' FUNCTION: read_mutant_selection_data
+#' Read the data file that contains the "mutant sample" data. This is the data file that allows us to compare mutation
+#' scores from the virtual mutation technique with scores when the original method is given the same amount of running
+#' time. Note that this data file is provided by the R package through the extdata directory.
 #' @export
-read_sample_file <- function() {
-  variable = c(1,2,3)
+read_mutant_selection_data <- function() {
+  f = system.file("extdata", "mutant_selection.dat", package="virtualmutationanalysis")
+  d = readr::read_csv(f)
+  return(dplyr::tbl_df(d))
 }
