70546df1df447b39659d58cffc0b8b457f9c3b7e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-17 22:23:15 -0500

    Adding really small font sizes and a new comment header to the visualise file.

--- R/visualise.R
+++ R/visualise.R
@@ -1,17 +1,17 @@
-#' FUNCTION: visualise_mutation_score
+#' FUNCTION: visualise_mutation_score_time_constrained
 #'
 #' Produces a visualisation shows the number of mutants in total and killed for the virtual mutation method and for the
 #' time-constrained original approach
 #' @export
 
-visualise_mutation_score <- function(data) {
+visualise_mutation_score_time_constrained <- function(data) {
   p = ggplot2::ggplot(data,ggplot2::aes(x = score_type, y = score)) +
     ggplot2::facet_grid(dbms~schema, labeller = ggplot2::label_parsed) +
     ggplot2::geom_boxplot(outlier.size = 0.75, lwd = 0.25) +
-    ggplot2::theme_grey(base_size = 12) +
-    ggplot2::theme(title = ggplot2::element_text(size=12)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 12)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 12)) +
+    ggplot2::theme_grey(base_size = 2) +
+    ggplot2::theme(title = ggplot2::element_text(size=2)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 2)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 2)) +
     ggplot2::xlab("Mutation Testing Technique") +
     ggplot2::ylab("Mutation Testing Score") +
     ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 23, size = 1, show_guide = FALSE)
