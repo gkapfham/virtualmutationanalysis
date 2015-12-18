8ab60c6a18079abe909fe1486efdd2b09c95f026
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-18 14:29:22 -0500

    Removing a use of  a deprecated variable inside of the visualise file.

--- R/visualise.R
+++ R/visualise.R
@@ -8,13 +8,22 @@ visualise_mutation_score_time_constrained <- function(data) {
   p = ggplot2::ggplot(data,ggplot2::aes(x = score_type, y = score)) +
     ggplot2::facet_grid(dbms~schema, labeller = ggplot2::label_parsed) +
     ggplot2::geom_boxplot(outlier.size = 0.75, lwd = 0.25) +
-    ggplot2::theme_grey(base_size = 2) +
-    ggplot2::theme(title = ggplot2::element_text(size=2)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 2)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 2)) +
+    ggplot2::theme_grey(base_size = 6) +
+    ggplot2::theme(title = ggplot2::element_text(size=6)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 4)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 4)) +
     ggplot2::xlab("Mutation Testing Technique") +
     ggplot2::ylab("Mutation Testing Score") +
-    ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 23, size = 1, show_guide = FALSE)
+    ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 23, size = 1, show.legend = FALSE)
   return(p)
+}
+
+#' FUNCTION: visualise_save_graphic
+#'
+#' Saves the provided graphic to the provided name.
+#' @export
+
+visualise_save_graphic <- function(save_name, save_plot) {
+  ggplot2::ggsave(save_name, save_plot, height=5, width=8)
 }
 
