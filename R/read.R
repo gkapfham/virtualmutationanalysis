5d949e798d8c1b6f5022dcce002665f47d14ef0d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-17 19:32:26 -0500

    Renaming files and making a wide variety of additional changes to virtualmutationanalysis.

--- R/read.R
+++ R/read.R
@@ -1,8 +1,10 @@
 #' FUNCTION: read_mutant_selection_data
+#'
 #' Read the data file that contains the "mutant sample" data. This is the data file that allows us to compare mutation
 #' scores from the virtual mutation technique with scores when the original method is given the same amount of running
 #' time. Note that this data file is provided by the R package through the extdata directory.
 #' @export
+
 read_mutant_selection_data <- function() {
   f = system.file("extdata", "mutant_selection.dat", package="virtualmutationanalysis")
   d = readr::read_csv(f)
