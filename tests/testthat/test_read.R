f915541cac0e3425cf28b15468c3b7c73bcd13fb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-17 20:05:13 -0500

    Updating comments and code in the test_read file.

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,8 +1,8 @@
 ### Context: test cases for reading in the data files
 context("read")
 
-test_that("The mutant_selection.dat file as a total of rows of data", {
-  mutant_selection_data <- read_mutant_selection_data()
-  expect_that(nrow(mutant_selection_data), equals(960))
+test_that("The time constrained mutation data file has a total of 960 !1080! rows of data", {
+  d <- read_time_constrained_mutation_data()
+  expect_that(nrow(d), equals(960))
 })
 
