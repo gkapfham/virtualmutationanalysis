33823825886030d5d5d119f405a1588a5f244964
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-18 15:44:01 -0500

    Adding in new test cases to test_transform; all are passing, for the new transform_time_constrained_data_for_total function.

--- tests/testthat/test_transform.R
+++ tests/testthat/test_transform.R
@@ -1,5 +1,5 @@
 ### Context: test cases for transforming data frames
-context("transform")
+context("transform-scores")
 
 test_that("The transformed data frame no longer contains certain attributes", {
   d <- read_time_constrained_mutation_data()
@@ -22,6 +22,36 @@ test_that("The transformed data frame now contains certain attributes", {
 test_that("The transformed data frame always contains certain attributes", {
   d <- read_time_constrained_mutation_data()
   dt <- transform_time_constrained_data_for_scores(d)
+  dc <- dt %>% select(contains("schema"))
+  expect_that(ncol(dc), equals(1))
+  dc <- dt %>% select(one_of(c("dbms")))
+  expect_that(ncol(dc), equals(1))
+})
+
+### Context: test cases for transforming data frames
+context("transform-totals")
+
+test_that("The transformed data frame no longer contains certain attributes", {
+  d <- read_time_constrained_mutation_data()
+  dt <- transform_time_constrained_data_for_totals(d)
+  dc <- dt %>% select(contains("vm_total"))
+  expect_that(ncol(dc), equals(0))
+  dc <- dt %>% select(contains("selection_total"))
+  expect_that(ncol(dc), equals(0))
+})
+
+test_that("The transformed data frame now contains certain attributes", {
+  d <- read_time_constrained_mutation_data()
+  dt <- transform_time_constrained_data_for_totals(d)
+  dc <- dt %>% select(contains("total_type"))
+  expect_that(ncol(dc), equals(1))
+  dc <- dt %>% select(one_of(c("total")))
+  expect_that(ncol(dc), equals(1))
+})
+
+test_that("The transformed data frame always contains certain attributes", {
+  d <- read_time_constrained_mutation_data()
+  dt <- transform_time_constrained_data_for_totals(d)
   dc <- dt %>% select(contains("schema"))
   expect_that(ncol(dc), equals(1))
   dc <- dt %>% select(one_of(c("dbms")))
