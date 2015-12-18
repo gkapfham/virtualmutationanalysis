78496cba33cc3784ea48ebb260cc110f90a7359a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-17 21:10:23 -0500

    Adding the importFrom command in the transform R file.

---
+++ R/transform.R
@@ -0,0 +1,19 @@
+#' FUNCTION: transform_time_constrained_data
+#'
+#' Transform the data that contains the virtual mutation scores and the time-constrained mutation scores so that they
+#' are "tidy" in format and thus easily amenable to visualization and analysis.
+#' @importFrom magrittr %>%
+#' @export
+
+transform_time_constrained_data_for_scores <- function(d) {
+  # go from data that is column-oriented to row-oriented
+  # change the name of the values to be better for graphing
+  tidy_data <- d %>%
+    dplyr::select(one_of(c("dbms", "schema", "vm_score", "selection_score"))) %>%
+    tidyr::gather(score_type, score, vm_score:selection_score) %>%
+    dplyr::mutate(score_type = plyr::revalue(score_type, c(
+            "vm_score" = "Virtual",
+            "selection_score" = "Time-Constrained"
+        )))
+  return(tidy_data)
+}
