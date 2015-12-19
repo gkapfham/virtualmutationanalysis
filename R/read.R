c82b4a2ed01b6925ae9e565ce5ade3d6ef4ddcab
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-19 15:13:22 -0500

    Adding in a new read function and fixing all of the assignment statement operators inside of the read file.

--- R/read.R
+++ R/read.R
@@ -6,7 +6,20 @@
 #' @export
 
 read_time_constrained_mutation_data <- function() {
-  f = system.file("extdata", "time_constrained_mutation.dat", package="virtualmutationanalysis")
-  d = readr::read_csv(f)
+  f <- system.file("extdata", "time_constrained_mutation.dat", package="virtualmutationanalysis")
+  d <- readr::read_csv(f)
+  return(dplyr::tbl_df(d))
+}
+
+#' FUNCTION: read_virtual_mutation_data
+#'
+#' Read the data file that contains the "virtual mutation" data. This is the data file that contains the data about
+#' virtual mutation (i.e., mutation using a model of the DBMS). Note that it contains data for more schemas than can
+#' actually be used in the AST 2016 paper submission.
+#' @export
+
+read_virtual_mutation_data <- function() {
+  f <- system.file("extdata", "virtual_mutation.dat", package="virtualmutationanalysis")
+  d <- readr::read_csv(f)
   return(dplyr::tbl_df(d))
 }
