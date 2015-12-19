b672cd75b2f6fcb98cb27f3dfb439db862cd33f2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-19 14:45:00 -0500

    Adding a test case that checks the value of the kappa correlation for our data.

--- tests/testthat/test_correlation.R
+++ tests/testthat/test_correlation.R
@@ -13,3 +13,10 @@ test_that("The correlation tests returns a p-value of zero for the time constrai
   expect_that(corr$p.value, equals(0))
 })
 
+test_that("The correlation tests returns a p-value of zero for the time constrained data", {
+  d <- read_time_constrained_mutation_data()
+  corr <- calculate_mutation_score_correlation(d)
+  expect_that(corr$estimate, is_less_than(1))
+  expect_that(corr$estimate, is_more_than(0))
+})
+
