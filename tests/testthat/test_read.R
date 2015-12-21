107c0cd975bb485e70f15e27aee5915daa31a95d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 11:28:49 -0500

    Commenting out a test case in test_read that cannot pass yet for awhile.

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,11 +1,12 @@
-### Context: test cases for reading in the data files
-context("read")
+### Context: test cases for reading in the data files for time constrained mutation
+context("read-time-constrained-mutation")
 
 test_that("The time constrained mutation data file has a total of 1440 rows of data", {
   d <- read_time_constrained_mutation_data()
   ns <- nrow(d %>% select(schema) %>% unique())
   nd <- nrow(d %>% select(dbms) %>% unique())
   nt <- 30
+  expect_that(ns*nd*nt, equals(1440))
   expect_that(nrow(d), equals(ns*nd*nt))
 })
 
@@ -13,4 +14,16 @@ test_that("The time constrained mutation data file has a total of 10 columns of 
   d <- read_time_constrained_mutation_data()
   expect_that(ncol(d), equals(10))
 })
+
+### Context: test cases for reading in the data files for time constrained mutation
+context("read-virtual-mutation")
+
+# test_that("The virtual mutation data file has a total of 2880 rows of data", {
+#   d <- read_virtual_mutation_data()
+#   ns <- nrow(d %>% select(schema) %>% unique())
+#   nd <- nrow(d %>% select(dbms) %>% unique())
+#   nt <- 30
+#   expect_that(ns*nd*nt, equals(2880))
+#   expect_that(nrow(d), equals(ns*nd*nt))
+# })
 
