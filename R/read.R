4af11d97e6215b4ffb5045b962c727607c618010
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 13:23:05 -0500

    Adding two new read functions to the read file, all tests for these are passing.

--- R/read.R
+++ R/read.R
@@ -25,6 +25,22 @@ read_virtual_mutation_data <- function() {
   return(dplyr::tbl_df(d))
 }
 
+#' FUNCTION: read_virtual_mutation_data_subset
+#'
+#' Read the data file that contains the "virtual mutation" data. This is the data file that contains the data about
+#' virtual mutation (i.e., mutation using a model of the DBMS). Data has been subset for the 16 chosen schemas for AST 2016.
+#' @importFrom magrittr %>%
+#' @export
+
+read_virtual_mutation_data_subset <- function() {
+  f <- system.file("extdata", "virtual_mutation.dat", package="virtualmutationanalysis")
+  m <- system.file("extdata", "time_constrained_mutation.dat", package="virtualmutationanalysis")
+  df <- readr::read_csv(f) %>% rename_mutation_for_attributes()
+  dm <- readr::read_csv(m)
+  df <- subset_chosen_schemas(df,dm)
+  return(dplyr::tbl_df(df))
+}
+
 #' FUNCTION: read_original_mutation_data
 #'
 #' Read the data file that contains the "original mutation" data. This is the data file that contains the data about
@@ -38,4 +54,20 @@ read_original_mutation_data <- function() {
   # 1. Read the CSV data file, 2. Use the standard naming convention, 3. Only use the avsDefaults data generator
   d <- readr::read_csv(f) %>% rename_mutation_for_attributes() %>% subset_data_generator()
   return(dplyr::tbl_df(d))
+}
+
+#' FUNCTION: read_original_mutation_data_subset
+#'
+#' Read the data file that contains the "original mutation" data. This is the data file that contains the data about
+#' original mutation (i.e., mutation that interacts with a DBMS). Data has been subset for the 16 chosen schemas for AST 2016.
+#' @importFrom magrittr %>%
+#' @export
+
+read_original_mutation_data_subset <- function() {
+  f <- system.file("extdata", "original_mutation.dat", package="virtualmutationanalysis")
+  m <- system.file("extdata", "time_constrained_mutation.dat", package="virtualmutationanalysis")
+  df <- readr::read_csv(f) %>% rename_mutation_for_attributes() %>% subset_data_generator()
+  dm <- readr::read_csv(m)
+  df <- subset_chosen_schemas(df,dm)
+  return(dplyr::tbl_df(df))
 }
