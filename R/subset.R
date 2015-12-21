dc2634c77773dc7712ebdd61b36265e663210b03
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 12:35:16 -0500

    Adding a new subset function for the schemas chosen for the AST 2016 paper.

--- R/subset.R
+++ R/subset.R
@@ -8,3 +8,15 @@ subset_data_generator <- function(d) {
   # remove all of the rows that are not for the avsDefaults data generator
   d %>% filter(datagenerator %in% c("avsDefaults"))
 }
+
+#' FUNCTION: subset_chosen_schemas
+#'
+#' Extract only those schemas that are part of the AST 2016 paper's empirical results.
+#' @export
+
+subset_chosen_schemas <- function(d, m) {
+  # subset a provided data frame (like the one for virtual or original mutation) so that it only contains the
+  # observations for the schemas present in the data frame called m (like the time constrained mutation data)
+  ds <- semi_join(d, m, by="schema")
+  return(ds)
+}
