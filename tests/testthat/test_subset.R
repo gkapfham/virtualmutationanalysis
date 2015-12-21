9be8dc84206040270ceb692c999e8d8ed701680e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 12:44:54 -0500

    Adding another (passing) test case to test_subset and fixing the test comment descriptors.

--- tests/testthat/test_subset.R
+++ tests/testthat/test_subset.R
@@ -1,22 +1,30 @@
 ### Context: test cases for subsetting the data frames
 context("subset")
 
-test_that("Subsetting the data leaves only the avsDefaults datagenerator", {
+test_that("Subsetting the data leaves only the avsDefaults datagenerator - original", {
   d <- read_original_mutation_data()
   ds <- d %>% subset_data_generator()
   dsd <- ds %>% select(datagenerator) %>% filter(datagenerator %in% c("avsDefaults"))  %>% unique()
   expect_that(nrow(dsd), equals(1))
 })
 
-test_that("Subsetting the data leaves only the avsDefaults datagenerator", {
+test_that("Subsetting the data leaves only the avsDefaults datagenerator - virtual", {
   d <- read_virtual_mutation_data()
   ds <- d %>% subset_data_generator()
   dsd <- ds %>% select(datagenerator) %>% filter(datagenerator %in% c("avsDefaults"))  %>% unique()
   expect_that(nrow(dsd), equals(1))
 })
 
-test_that("Subsetting the data leaves only the chosen schemas for the AST 2016 paper", {
+test_that("Subsetting the data leaves only the chosen schemas for the AST 2016 paper - original", {
   d <- read_original_mutation_data()
+  m <- read_time_constrained_mutation_data()
+  ds <- subset_chosen_schemas(d, m)
+  dss <- ds %>% select(schema) %>% unique()
+  expect_that(nrow(dss), equals(16))
+})
+
+test_that("Subsetting the data leaves only the chosen schemas for the AST 2016 paper - virtual", {
+  d <- read_virtual_mutation_data()
   m <- read_time_constrained_mutation_data()
   ds <- subset_chosen_schemas(d, m)
   dss <- ds %>% select(schema) %>% unique()
