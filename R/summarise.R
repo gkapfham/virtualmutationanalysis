1df7d04c7461815cb15d8e2d4eee15ff05cdc287
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 11:16:37 -0500

    Adding the summarise file with the first summarise function to assist in debugging.

---
+++ R/summarise.R
@@ -0,0 +1,10 @@
+#' FUNCTION: summarise_unique_attributes
+#'
+#' Renames the mutation data so that it uses the correct attribute names.
+#' @importFrom magrittr %>%
+#' @export
+
+summarise_unique_attributes <- function(d) {
+  # this summarise_each makes it easier to see the unique levels for each attribute
+  d %>% summarise_each(funs(n_distinct)) %>% glimpse()
+}
