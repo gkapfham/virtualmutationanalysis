7917ee6a6119577c7900c4b5a326e3e32a2ee320
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 11:38:31 -0500

    Fixing the context in the test_subset and ensuring that the test case now passes.

--- tests/testthat/test_subset.R
+++ tests/testthat/test_subset.R
@@ -1,10 +1,10 @@
 ### Context: test cases for subsetting the data frames
-context("correlation")
+context("subset")
 
 test_that("Subsetting the data leaves only the avsDefaults datagenerator", {
   d <- read_original_mutation_data()
   ds <- d %>% subset_data_generator()
-  dsd <- ds %>% filter(datagenerator %in% c("avsDefaults")) %>% unique()
+  dsd <- ds %>% select(datagenerator) %>% filter(datagenerator %in% c("avsDefaults"))  %>% unique()
   expect_that(nrow(dsd), equals(1))
 })
 
