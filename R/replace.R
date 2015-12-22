ccab96468683f7502f574512a7ea47faed5106ed
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 22:55:24 -0500

    Adding in a new replace_virtual_technique to the replace file.

--- R/replace.R
+++ R/replace.R
@@ -1,6 +1,6 @@
 #' FUNCTION: replace_original_technique
 #'
-#' Replace the original technique name in the original data set.
+#' Replace the original technique name in the original data set with a name that is properly descriptive.
 #' @importFrom magrittr %>%
 #' @export
 
@@ -8,5 +8,18 @@ replace_original_technique <- function(d) {
   # replace the label "mutantTiming" with the label "Original"
   replaced_d <- d %>% mutate(technique = factor(technique)) %>%
     mutate(technique = revalue(technique, c("mutantTiming" = "Original")))
+  return(replaced_d)
+}
+
+#' FUNCTION: replace_virtual_technique
+#'
+#' Replace the virtual technique name in the original data set with a name that is capitalized.
+#' @importFrom magrittr %>%
+#' @export
+
+replace_virtual_technique <- function(d) {
+  # replace the label "virtual" with the label "Virtual"
+  replaced_d <- d %>% mutate(technique = factor(technique)) %>%
+    mutate(technique = revalue(technique, c("virtual" = "Virtual")))
   return(replaced_d)
 }
