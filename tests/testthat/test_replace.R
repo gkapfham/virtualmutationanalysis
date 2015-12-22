a4699c7c5462d65dda522e9cdc4ac7e9dd589b01
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 22:50:21 -0500

    Adding more test cases to the test_replace file and changing the name of the context.

--- tests/testthat/test_replace.R
+++ tests/testthat/test_replace.R
@@ -1,10 +1,17 @@
 ### Context: test cases for reading in the data file for the virtual mutation data
-context("replace-original-mutation")
+context("replace-mutation-technique")
 
 test_that("Replacement on original mutation gives the correct technique name", {
   d <- read_original_mutation_data_subset()
   dr <- d %>% replace_original_technique()
   drw <- dr %>% filter(technique %in% c("mutantTiming")) %>% distinct()
+  expect_that(nrow(drw), equals(0))
+})
+
+test_that("Replacement on virtual mutation gives the correct technique name", {
+  d <- read_virtual_mutation_data_subset()
+  dr <- d %>% replace_virtual_technique()
+  drw <- dr %>% filter(technique %in% c("virtual")) %>% distinct()
   expect_that(nrow(drw), equals(0))
 })
 
