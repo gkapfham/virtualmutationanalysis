8060b64fcbceaa165d4d1434e54134078b948284
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-22 18:16:13 -0500

    Adding in the first new summarise function for the postgres original data.

--- R/summarise.R
+++ R/summarise.R
@@ -6,5 +6,29 @@
 
 summarise_unique_attributes <- function(d) {
   # summarise_each makes it easier to see the unique levels for each attribute
-  d %>% summarise_each(funs(n_distinct)) %>% glimpse()
+  d %>% dplyr::summarise_each(funs(dplyr::n_distinct)) %>% dplyr::glimpse()
 }
+
+#' FUNCTION: summarise_unique_attributes
+#'
+#' Renames the mutation data so that it uses the correct attribute names.
+#' @importFrom magrittr %>%
+#' @export
+
+summarise_mutation_analysis <- function(d) {
+  # summarise the data to get the analysis time for original and the denominator
+  s = d %>% dplyr::group_by(identifier, schema, dbms) %>%
+    dplyr::filter(type %in% c("NORMAL")) %>%
+    dplyr::summarise(mutationanalysistime=sum(time), denominator=n()) %>%
+    dplyr::ungroup()
+  # summarise the data to get the numerator (note the extra filter on the killed status)
+  n = d %>% dplyr::group_by(identifier, schema, dbms) %>%
+    dplyr::filter(type %in% c("NORMAL")) %>%
+    dplyr::filter(killed %in% c("true")) %>%
+    dplyr::summarise(numerator=n()) %>%
+    dplyr::ungroup()
+  # join the two previous data frames together into a single data frame
+  ns = dplyr::right_join(s, n, c("identifier" = "identifier", "schema" = "schema", "dbms" = "dbms"))
+  return(ns)
+}
+
