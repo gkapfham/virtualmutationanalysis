19f85c7daaf3bf1acd53a91829bdff77bd8edd47
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-22 16:15:45 -0500

    Adding in the tests, read function, and data for the HyperSQL data file.

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -79,4 +79,16 @@ test_that("The original mutation (Postgres) data file has a total of nine schema
   expect_that(nd, equals(1))
 })
 
+### Context: test cases for reading in the data file for original mutation data (HyperSQL)
+context("read-original-mutation-hypersql")
+
+test_that("The original mutation (HyperSQL) data file has a total of nine schemas and one dbms", {
+  d <- read_original_mutation_data_hypersql_subset()
+  ns <- nrow(d %>% select(schema) %>% unique())
+  nd <- nrow(d %>% select(dbms) %>% unique())
+  expect_that(ns, equals(9))
+  expect_that(nd, equals(1))
+})
+
+
 
