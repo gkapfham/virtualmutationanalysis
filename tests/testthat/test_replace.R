87231c40cbaf20f741662a3888ec0049d71b5b00
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-21 22:52:40 -0500

    Updating one test and adding another test to the test_replace file.

--- tests/testthat/test_replace.R
+++ tests/testthat/test_replace.R
@@ -1,11 +1,18 @@
 ### Context: test cases for reading in the data file for the virtual mutation data
 context("replace-mutation-technique")
 
+test_that("Replacement on original mutation does not give the old technique name", {
+  d <- read_original_mutation_data_subset()
+  dr <- d %>% replace_original_technique()
+  drw <- dr %>% filter(technique %in% c("mutantTiming")) %>% select(technique) %>% distinct()
+  expect_that(nrow(drw), equals(0))
+})
+
 test_that("Replacement on original mutation gives the correct technique name", {
   d <- read_original_mutation_data_subset()
   dr <- d %>% replace_original_technique()
-  drw <- dr %>% filter(technique %in% c("mutantTiming")) %>% distinct()
-  expect_that(nrow(drw), equals(0))
+  drw <- dr %>% filter(technique %in% c("Original")) %>% select(technique) %>% distinct()
+  expect_that(nrow(drw), equals(1))
 })
 
 test_that("Replacement on virtual mutation gives the correct technique name", {
