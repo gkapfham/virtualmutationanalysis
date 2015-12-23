cd7a9e1730a0b3280e14b20cee4e4e0fab5d0145
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-22 23:01:55 -0500

    Updating code in the summarise file so that functions give different names to attributes when summarising the original data from three DBMS files.

--- R/summarise.R
+++ R/summarise.R
@@ -19,13 +19,13 @@ summarise_mutation_analysis <- function(d) {
   # summarise the data to get the analysis time for original and the denominator
   s = d %>% dplyr::group_by(identifier, schema, dbms) %>%
     dplyr::filter(type %in% c("NORMAL")) %>%
-    dplyr::summarise(mutationanalysistime=sum(time), denominator=n()) %>%
+    dplyr::summarise(mutationanalysistime=sum(time), scoredenominator=n()) %>%
     dplyr::ungroup()
   # summarise the data to get the numerator (note the extra filter on the killed status)
   n = d %>% dplyr::group_by(identifier, schema, dbms) %>%
     dplyr::filter(type %in% c("NORMAL")) %>%
     dplyr::filter(killed %in% c("true")) %>%
-    dplyr::summarise(numerator=n()) %>%
+    dplyr::summarise(scorenumerator=n()) %>%
     dplyr::ungroup()
   # join the two previous data frames together into a single data frame
   ns = dplyr::right_join(s, n, c("identifier" = "identifier", "schema" = "schema", "dbms" = "dbms"))
