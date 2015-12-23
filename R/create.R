e67633f81efe5011fc170d271a9de6fe1fe41e07
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-22 22:52:56 -0500

    Changing function names and starting a new function inside of the create file.

--- R/create.R
+++ R/create.R
@@ -1,14 +1,26 @@
-#' FUNCTION: create_dbms_data
+#' FUNCTION: create_original_data
 #'
-#' Create the data from the HyperSQL, Postgres, and SQLite databases.
+#' Create the "original" data from the HyperSQL, Postgres, and SQLite databases.
 #' @importFrom magrittr %>%
 #' @export
 
-create_dbms_data <- function(h, p, s) {
+create_original_data <- function() {
+  # create the data frames for the three individual database management systems
   h <- read_original_mutation_data_hypersql_subset() %>% summarise_mutation_analysis() %>% subset_all_attributes_except_identifier()
   p <- read_original_mutation_data_postgres_subset() %>% summarise_mutation_analysis() %>% subset_all_attributes_except_identifier()
   s <- read_original_mutation_data_sqlite_subset() %>% summarise_mutation_analysis() %>% subset_all_attributes_except_identifier()
+  # combine these three data frames into a single data frame
   hps <- combine_dbms_data(h, p, s)
+  # add in an attribute signalling that this is the Original data set
   hps <- hps %>% dplyr::mutate(technique="Original")
   return(hps)
+}
+
+#' FUNCTION: create_dbms_data
+#'
+#' Create virtual data from the virtual data file, then transform it and munge it into the right format.
+#' @importFrom magrittr %>%
+#' @export
+
+create_virtual_data <- function() {
 }
