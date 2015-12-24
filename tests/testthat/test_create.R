e70e9011822b2155076fcec3f40afe52a5af9a19
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-24 10:15:56 -0500

    Adding more test contexts and test cases to the test_create file.

--- tests/testthat/test_create.R
+++ tests/testthat/test_create.R
@@ -7,9 +7,21 @@ test_that("Combining the three data sets together yields the correct size data s
   expect_that(nrow(hps), equals(270*3))
 })
 
+### Context: test cases for creating the combined data frames across the three DBMSs
+context("create-virtual-data")
+
 test_that("Creating the virtual data yields the correct size data set", {
   hps <- create_virtual_data()
   expect_that(ncol(hps), equals(6))
   expect_that(nrow(hps), equals(270*3))
+})
+
+### Context: test cases for creating the combined data frames across the three DBMSs
+context("create-original-virtual-data")
+
+test_that("Creating the virtual data yields the correct size data set", {
+  hps <- create_original_and_virtual_data()
+  expect_that(ncol(hps), equals(6))
+  expect_that(nrow(hps), equals((270*3)+(270*3)))
 })
 
