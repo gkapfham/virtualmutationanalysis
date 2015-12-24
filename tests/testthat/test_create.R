33e6203642829107dc5350d781bf040b0842ae8b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2015-12-24 10:06:35 -0500

    Updating the test cases in the test_create file. All are passing now.

--- tests/testthat/test_create.R
+++ tests/testthat/test_create.R
@@ -3,6 +3,13 @@ context("create-original-dbmss")
 
 test_that("Combining the three data sets together yields the correct size data set", {
   hps <- create_original_data()
+  expect_that(ncol(hps), equals(6))
+  expect_that(nrow(hps), equals(270*3))
+})
+
+test_that("Creating the virtual data yields the correct size data set", {
+  hps <- create_virtual_data()
+  expect_that(ncol(hps), equals(6))
   expect_that(nrow(hps), equals(270*3))
 })
 
