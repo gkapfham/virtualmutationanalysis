d426b1ddc591eccc4926dd931c344d15c385ac68
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-01-05 22:43:15 -0500

    Increasing slightly the font size used in the box and whisker plot for the function in the visualise file.

--- R/visualise.R
+++ R/visualise.R
@@ -10,8 +10,8 @@ visualise_mutation_score_time_constrained <- function(data) {
     ggplot2::geom_boxplot(outlier.size = 0.75, lwd = 0.25) +
     ggplot2::theme_grey(base_size = 6) +
     ggplot2::theme(title = ggplot2::element_text(size=6)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 4)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 4)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 5)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 5)) +
     ggplot2::xlab("Database Schema") +
     ggplot2::ylab("Mutation Score") +
     ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 23, size = 1, show.legend = FALSE)
