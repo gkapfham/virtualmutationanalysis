bfe56088ade4552146d146bb93c37b839108bc47
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-01-10 20:42:25 -0500

    Starting to add in the code that breaks the statistical analysis out by the separate DBMS. Output is working correctly.

--- R/analyse.R
+++ R/analyse.R
@@ -1,7 +1,8 @@
 #' FUNCTION: analyse_wilcox_rank_sum_test
 #'
 
-#' Perform a statistical analysis of the timings for the standard and virtual mutation analysis techniques.
+#' Perform a statistical analysis of the timings for the standard and virtual mutation analysis techniques. Right now, this method
+#' simply returns textual output; it would be much better if it actually returned a single data frame.
 #' @importFrom magrittr %>%
 #' @export
 
@@ -11,12 +12,18 @@ analyse_wilcox_rank_sum_test <- function(d) {
   # extract the relevant data values for the virtual method
   virtual_time <- d %>% dplyr::filter(technique %in% c("Virtual"))
 
-  # extract the relevant data values for the HyperSQL database
+  # extract and analyse the relevant data values for the HyperSQL database
   standard_time_hypersql <- analyse_extract_dbms_data(standard_time, "HyperSQL")
   virtual_time_hypersql <- analyse_extract_dbms_data(virtual_time, "HyperSQL")
-
   standard_time_hypersql %>% dplyr::distinct(dbms) %>% dplyr::select(dbms) %>% glimpse()
-  analyse_perform_wilcox_rank_sum_test(standard_time_hypersql, virtual_time_hypersql)
+  analyse_perform_wilcox_rank_sum_test(standard_time_hypersql, virtual_time_hypersql) %>% glimpse()
+
+  # extract and analyse the relevant data values for the PostgreSQL database
+  standard_time_postgres <- analyse_extract_dbms_data(standard_time, "PostgreSQL")
+  virtual_time_postgres <- analyse_extract_dbms_data(virtual_time, "PostgreSQL")
+  standard_time_postgres %>% dplyr::distinct(dbms) %>% dplyr::select(dbms) %>% glimpse()
+  analyse_perform_wilcox_rank_sum_test(standard_time_postgres, virtual_time_postgres) %>% glimpse()
+
 
 }
 
