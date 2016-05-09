d0fe98510981b808c9ccff17068c2b28462a8455
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-05-09 14:22:56 -0400

    Updating the font sizes for the color graphics to be used in the presentations.

--- R/visualise.R
+++ R/visualise.R
@@ -28,10 +28,10 @@ visualise_mutation_score_time_constrained_color <- function(data) {
   p <- ggplot2::ggplot(data,ggplot2::aes(x = schema, y = score, fill = score_type)) +
     ggplot2::facet_grid(dbms~score_type, labeller = ggplot2::label_parsed) +
     ggplot2::geom_boxplot(outlier.size = 0.75, lwd = 0.25) +
-    ggplot2::theme_bw(base_size = 6) +
+    ggplot2::theme_bw(base_size = 8) +
     ggplot2::theme(title = ggplot2::element_text(size=6)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 5)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 5)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 6)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 6)) +
     ggplot2::theme(strip.background = element_blank(), panel.border = element_rect(colour = "black")) +
     ggplot2::scale_fill_brewer(palette="Set2", guide=FALSE) +
     ggplot2::xlab("Database Schema") +
